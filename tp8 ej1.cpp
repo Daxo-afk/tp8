a4c720a85a37535f24ebaca7b1debc402a7e11e2
Dante <danterossoatkinson@gmail.com>
2025-11-27 08:33:58 -0300

    Add files via upload

---
+++ tp8 ej1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <algorithm>
+#include <string.h>
+using namespace std;
+int main(){
+	
+	int vector[4] ={3, 5, 1, 7};
+	int nidea;
+	nidea = vector[1] + vector[2] + vector[3] + vector[4];
+	cout<<"El valor de la suma de cada elemento de el vector es"<<nidea;
+
+	return 0;
+}
